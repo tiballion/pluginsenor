1dc4c668a065be09bc8788eb703c225563619825
tiballion <timothee.ballion@etu.u-bordeaux.fr>
2022-09-18 21:18:58 +0200

    updated CommandSetSpawn.java : now able to set the spawn using coordinates.

--- src/main/java/vorkubinks/pluginsenor/commands/CommandSetSpawn.java
+++ src/main/java/vorkubinks/pluginsenor/commands/CommandSetSpawn.java
@@ -1,6 +1,8 @@
 package vorkubinks.pluginsenor.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,17 +17,25 @@ public class CommandSetSpawn implements CommandExecutor {
         this.pluginsenor = pluginsenor;
     }
 
+    private void setSpawn(Location location, Player player) {
+        pluginsenor.getConfig().set("spawn", location);
+        pluginsenor.saveConfig();
+        player.sendMessage("Spawn location has been set.");
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player) {
             Player player = (Player) sender;
+            Location location = player.getLocation();
             if(args.length == 0) {
-                Location location = player.getLocation();
-                pluginsenor.getConfig().set("spawn", location);
-                pluginsenor.saveConfig();
-                player.sendMessage("Spawn location has been set");
+                setSpawn(location, player);
             } else if(args.length == 3) {
-                //Location location =
+                double locX = Double.parseDouble(args[0]);
+                double locY = Double.parseDouble(args[1]);
+                double locZ = Double.parseDouble(args[2]);
+                Location loc = new Location(location.getWorld(), locX, locY, locZ);
+                setSpawn(loc, player);
             } else {
                 player.sendMessage("This command expects either 0 or 3 parameters (x, y, z)");
             }
